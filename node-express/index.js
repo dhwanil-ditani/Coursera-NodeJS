8801890588829aa3f183a3151e6147b6e4ea74fd
dhwanil-ditani <ditanidhwanil@gmail.com>
2021-07-10 08:34:56 +0000

    Assignment 1

--- node-express/index.js
+++ node-express/index.js
@@ -1,53 +1,34 @@
-const express = require('express')
-const http = require('http');
+const http = require("http");
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-const morgan = require('morgan');
 
-const bodyParser = require('body-parser');
+const dishRouter = require("./routes/dishRouter");
+const leaderRouter = require("./routes/leaderRouter");
+const promoRouter = require("./routes/promoRouter");
 
-const dishRouter = require('./routes/dishRouter');
-
-const hostname = 'localhost';
+const hostname = "localhost";
 const port = 8080;
 
 const app = express();
 
 app.use(morgan('dev'));
-
-app.use(express.static(__dirname + '/public'));
-
 app.use(bodyParser.json());
 
-// app.get('/dishes/:dishId', (req,res,next) => {
-//     res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
-// });
+app.use("/dishes", dishRouter);
+app.use("/leaders", leaderRouter);
+app.use("/promotions", promoRouter);
 
-// app.post('/dishes/:dishId', (req, res, next) => {
-//   res.statusCode = 403;
-//   res.end('POST operation not supported on /dishes/'+ req.params.dishId);
-// });
-
-// app.put('/dishes/:dishId', (req, res, next) => {
-//   res.write('Updating the dish: ' + req.params.dishId + '\n');
-//   res.end('Will update the dish: ' + req.body.name + 
-//         ' with details: ' + req.body.description);
-// });
-
-// app.delete('/dishes/:dishId', (req, res, next) => {
-//     res.end('Deleting dish: ' + req.params.dishId);
-// });
-
-app.use('/dishes', dishRouter);
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, res, next) => {
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+    res.statusCode = 200;
+    res.setHeader("Content-type", "text/html");
+    res.end("<html><body><h1>This is an express server</h1></body></html>");
 });
 
 const server = http.createServer(app);
-
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log("Server is running at https://" + hostname + ":" + port + "/");
 });
